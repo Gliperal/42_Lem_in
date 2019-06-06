6c47daa893fc74db739faf6fa387f27c1b212d01
Nickolai Whitlow <ananasviolet@fintmcwells.com>
2019-06-06 12:40:42 -0700

    Some norming

--- backtracking.c
+++ backtracking.c
@@ -6,7 +6,7 @@
 /*   By: nwhitlow <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 21:12:40 by nwhitlow          #+#    #+#             */
-/*   Updated: 2019/06/06 12:11:56 by nwhitlow         ###   ########.fr       */
+/*   Updated: 2019/06/06 12:31:17 by nwhitlow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,45 +16,51 @@
 
 int	next_path(t_graph *graph, t_arrlst *path, t_arrlst *blocked_nodes);
 
-static int	backtrack(t_graph *graph, t_arrlst *paths, t_arrlst *blocked_nodes, int num_paths)
+static int	bt(t_graph *graph, t_arrlst *paths, t_arrlst *used, int num_paths)
 {
+	t_arrlst	*new_path;
+	int			status;
+
 	if (num_paths == 0)
 		return (1);
-	t_arrlst *new_path = ft_arrlst_new(sizeof(int));
+	new_path = ft_arrlst_new(sizeof(int));
 	if (!new_path)
 		return (0);
 	ft_arrlst_add(new_path, &(graph->start));
 	ft_arrlst_add(paths, &new_path);
 	while (1)
 	{
-		int status = next_path(graph, new_path, blocked_nodes);
+		status = next_path(graph, new_path, used);
 		if (status == 0)
 		{
 			ft_arrlst_remove_last(paths, 1);
 			return (0);
 		}
-		ft_arrlst_add_arrlst(blocked_nodes, new_path, 1, new_path->size - 2);
-		if (backtrack(graph, paths, blocked_nodes, num_paths - 1))
+		ft_arrlst_add_arrlst(used, new_path, 1, new_path->size - 2);
+		if (bt(graph, paths, used, num_paths - 1))
 			return (1);
-		ft_arrlst_remove_last(blocked_nodes, new_path->size - 2);
+		ft_arrlst_remove_last(used, new_path->size - 2);
 	}
 }
 
 t_arrlst	*create_paths(t_graph *graph, int num_paths)
 {
+	t_arrlst *paths;
+	t_arrlst *blocked_nodes;
+
 	// num_paths can be capped by degree of start and end as well (and number of ants)
 	if (num_paths < 0)
 		return (NULL);
-	t_arrlst *paths = ft_arrlst_new(sizeof(t_arrlst *));
+	paths = ft_arrlst_new(sizeof(t_arrlst *));
 	if (!paths)
 		return (NULL);
-	t_arrlst *blocked_nodes = ft_arrlst_new(sizeof(int));
+	blocked_nodes = ft_arrlst_new(sizeof(int));
 	if (!blocked_nodes)
 	{
 		ft_arrlst_del(&paths);
 		return (0);
 	}
-	if (backtrack(graph, paths, blocked_nodes, num_paths))
+	if (bt(graph, paths, blocked_nodes, num_paths))
 		return (paths);
 	return (NULL);
 }
--- backtracking_paths.c
+++ backtracking_paths.c
@@ -6,7 +6,7 @@
 /*   By: nwhitlow <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 17:43:53 by nwhitlow          #+#    #+#             */
-/*   Updated: 2019/06/06 12:20:14 by nwhitlow         ###   ########.fr       */
+/*   Updated: 2019/06/06 12:35:50 by nwhitlow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,36 @@
 #include "node.h"
 #include "libft/libft.h"
 
-int complete_path(t_graph *graph, t_arrlst *path, t_arrlst *blocked_nodes)
+int	complete_path(t_graph *graph, t_arrlst *path, t_arrlst *blocked_nodes)
 {
+	int		i;
+	t_node	*node;
+
 	if (!graph || !path || !blocked_nodes)
 		return (0);
-	int i = *(int *)ft_arrlst_get(path, path->size - 1);
+	i = *(int *)ft_arrlst_get(path, path->size - 1);
 	if (i == graph->end)
 		return (1);
-	t_node *node = graph->nodes[i];
+	node = graph->nodes[i];
 	i = 0;
 	while (i < node->degree)
 	{
-		if (ft_arrlst_contains(blocked_nodes, node->neighbors + i) ||
-				ft_arrlst_contains(path, node->neighbors + i)
-		)
+		if (!ft_arrlst_contains(blocked_nodes, node->neighbors + i) &&
+				!ft_arrlst_contains(path, node->neighbors + i))
 		{
-			i++;
-			continue ;
+			ft_arrlst_add(path, node->neighbors + i);
+			if (node->neighbors[i] == graph->end)
+				return (1);
+			if (complete_path(graph, path, blocked_nodes))
+				return (1);
+			ft_arrlst_remove_last(path, 1);
 		}
-		ft_arrlst_add(path, node->neighbors + i);
-		if (node->neighbors[i] == graph->end)
-			return (1);
-		if (complete_path(graph, path, blocked_nodes))
-			return (1);
-		ft_arrlst_remove_last(path, 1);
 		i++;
 	}
 	return (0);
 }
 
-int neighbor_index(t_node *node, int neighbor)
+int	neighbor_index(t_node *node, int neighbor)
 {
 	int i;
 
@@ -57,16 +57,10 @@ int neighbor_index(t_node *node, int neighbor)
 	return (-1);
 }
 
-void	ft_arrlst_set(t_arrlst *lst, int index, void *data)
-{
-	ft_memcpy(lst->data + index * lst->elem_size, data, lst->elem_size);
-}
-
 int	next_path(t_graph *graph, t_arrlst *path, t_arrlst *blocked_nodes)
 {
 	int index;
 	t_node *node;
-	int *neighbors;
 	int i;
 
 	if (*(int *)ft_arrlst_get(path, path->size - 1) != graph->end)
@@ -75,19 +69,16 @@ int	next_path(t_graph *graph, t_arrlst *path, t_arrlst *blocked_nodes)
 	while (index >= 0)
 	{
 		node = graph->nodes[*(int *)ft_arrlst_get(path, index)];
-		neighbors = node->neighbors;
 		i = neighbor_index(node, *(int *)ft_arrlst_get(path, index + 1)) + 1;
 		while (i < node->degree)
 		{
-			if (ft_arrlst_contains(blocked_nodes, node->neighbors + i) ||
-					ft_arrlst_contains(path, node->neighbors + i))
+			if (!ft_arrlst_contains(blocked_nodes, node->neighbors + i) &&
+					!ft_arrlst_contains(path, node->neighbors + i))
 			{
-				i++;
-				continue ;
+				ft_arrlst_set(path, path->size - 1, node->neighbors + i);
+				if (complete_path(graph, path, blocked_nodes))
+					return (1);
 			}
-			ft_arrlst_set(path, path->size - 1, neighbors + i);
-			if (complete_path(graph, path, blocked_nodes))
-				return (1);
 			i++;
 		}
 		ft_arrlst_remove_last(path, 1);
